41627782885779c958a777693277fee1aa0c9ac2
ekaro <marek.pekarovic@gmail.com>
2017-11-28 20:49:28 +0100

    added DrawDeck and DrawCard functions

--- WindowApp/Deck.cpp
+++ WindowApp/Deck.cpp
@@ -11,7 +11,7 @@ void Deck::GenerateDeck()
 {
 	int index = 0;
 	int x = 0;
-	int y = 0;
+	int y = offset;
 	COLORREF Green = (RGB(0, 255, 0));
 
 	for (int i = 0; i < rows; i++)
@@ -27,10 +27,25 @@ void Deck::GenerateDeck()
 	}
 }
 
+void Deck::DrawDeck(HDC hdc)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		Cards[i].DrawCard(hdc);
+	}
+}
+
 Deck::Card::Card(int x, int y, COLORREF CardColor)
 	:
 	left(x),
 	top(y),
 	color(CardColor)
 {
+}
+
+void Deck::Card::DrawCard(HDC hdc)
+{
+	SelectObject(hdc, GetStockObject(DC_BRUSH));
+	SetDCBrushColor(hdc, color);
+	Rectangle(hdc, left, top, left + CardWidth, top + CardHeight);
 }
--- WindowApp/Deck.h
+++ WindowApp/Deck.h
@@ -9,20 +9,24 @@ private:
 	{
 	public:
 		Card(int x, int y, COLORREF CardColor);
-		void DrawCard();
+		void DrawCard(HDC hdc);
 		int CardWidth = 100;
 		int CardHeight = 150;
 		COLORREF color;
 	private:
+		HDC handle;
 		int left;
 		int top;		
 	};
 public:
 	Deck(HDC hdc, int ButtonOffset);
 	void GenerateDeck();
+	void DrawDeck(HDC hdc);
 private:
 	HDC handle;
 	int offset;
+	int x;
+	int y;
 	int rows = 4;
 	int columns = 5;
 	std::vector<Card> Cards;
